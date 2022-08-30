9252b0815a57bcbc86fb19048c2599466035d35e
Holden-VanKeyes <102389369+Holden-VanKeyes@users.noreply.github.com>
2022-08-30 09:04:17 -0500

    commit tree b4bbae31172dfe83b91fd8a1b0508247fa7f2eee

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,44 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case 'friends/add':
+      let newState = action.payload
+
+      return {
+        friends: [
+          ...state.friends,
+          {
+            name: newState.name,
+            hometown: newState.hometown,
+            id: newState.id,
+          },
+        ],
+      }
+    case 'friends/remove':
+      let friend = action.payload
+      console.log(state.friends)
+      return {
+        friends: [...state.friends.splice(0, friend.id)],
+      }
+
+    default:
+      return state
+  }
 }
+
+// {friends: []}, action){
+
+//   switch(action.type) {
+//     case 'ADD_FRIEND':
+//       return {
+//         friends: [
+//           // should output this
+//           // { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Joe', hometown: 'Boston', id: 101 }
+//         ]
+//       }
+//     case 'REMOVE_FRIEND':
+//       return [
+//         // remove friend
+//       ]
+//     default:
+//       return state
+//   }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,7 @@
-const initialState = { numberOfPresents: 0 };
+const initialState = { numberOfPresents: 0 }
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  if (action.type === 'presents/increase') {
+    return { numberOfPresents: state.numberOfPresents + 1 }
+  } else return state
 }
